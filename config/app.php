1b135f0f98436e6724f6009b4e671cc066d5dd57
Santi1997 <syepes786@misena.edu.co>
2017-04-20 17:27:59 -0500

    app

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
     Jleon\LaravelPnotify\NotifyServiceProvider::class,
     Yajra\Datatables\DatatablesServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
-
+    Maatwebsite\Excel\ExcelServiceProvider::class,
     
 
   ],
@@ -237,7 +237,7 @@ return [
     'Html' => Collective\Html\HtmlFacade::class,
     'Notify' => Jleon\LaravelPnotify\Notify::class,
     'Datatables' => yajra\Datatables\Datatables::class,
-    
+    'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     'PDF' => Barryvdh\DomPDF\Facade::class,
 
 
